0fb8f5e3ce4ecf8270703e2ba9d6d379b4f89ad4
Soroush Beigi <sbeygi712@gmail.com>
2025-02-27 18:43:29 +0330

    feat: implemented shared_preferences local storage for auth

--- lib/core/data_sources/user_local_data_source.dart
+++ lib/core/data_sources/user_local_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:abantether/features/auth/domain/entities/auth.dart';
 
 abstract class UserLocalDataSource {
+  Future<void> init();
   Future<void> writeToken(Auth auth);
   Future<Auth> readToken();
 }
--- lib/core/data_sources/user_local_data_source_impl.dart
+++ lib/core/data_sources/user_local_data_source_impl.dart
@@ -1,23 +1,38 @@
 import 'package:abantether/core/constants/app_constants.dart';
 import 'package:abantether/core/data_sources/user_local_data_source.dart';
 import 'package:abantether/features/auth/domain/entities/auth.dart';
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+//Alternative secure way by using flutter_secure_storage is commented in this file
 @LazySingleton(as: UserLocalDataSource)
 class UserLocalDataSourceImpl extends UserLocalDataSource {
-  // final storage = const FlutterSecureStorage();
+  SharedPreferences? prefs;
+  //final storage = const FlutterSecureStorage();
   UserLocalDataSourceImpl();
 
   @override
-  Future<Auth> readToken() async{
+  Future<Auth> readToken() async {
     // final token = await storage.read(key: tokenLocalKey);
-    return Auth(authToken: 'token');
+    // return Auth(authToken: token);
+    if (prefs == null) {
+      await init();
+    }
+    final token = prefs!.getString(tokenLocalKey);
+    return Auth(authToken: token);
   }
 
   @override
-  Future<void> writeToken(Auth auth)async {
+  Future<void> writeToken(Auth auth) async {
     // await storage.write(key: tokenLocalKey, value: auth.authToken);
+    if (prefs == null) {
+      await init();
+    }
+    await prefs!.setString(tokenLocalKey, auth.authToken ?? '');
   }
 
+  @override
+  Future<void> init() async {
+    prefs = await SharedPreferences.getInstance();
+  }
 }
